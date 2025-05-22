8281ff4a9e8e481d803408d23572c635e9ab7e52
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 15:09:23 +0200

    added relationship one to many between Organizzazione and RagioneSociale

--- app/Models/RagioneSociale.php
+++ app/Models/RagioneSociale.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RagioneSociale extends Model
 {
@@ -21,4 +22,9 @@ class RagioneSociale extends Model
         'email',
         'tel',
     ];
+
+    public function organizzazione(): BelongsTo
+    {
+        return $this->belongsTo(Organizzazione::class);
+    }
 }
