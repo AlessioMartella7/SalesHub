1cec6bae2a62f9778ed6e8992ed851673111c675
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 15:55:17 +0200

    added relationship one to many between Vendita and Pagamento

--- app/Models/Pagamento.php
+++ app/Models/Pagamento.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pagamento extends Model
 {
@@ -27,4 +28,9 @@ class Pagamento extends Model
         'importo_conto_operatore_contanti',
         'importo_conto_operatore_pos',
     ];
+
+    public function vendita(): BelongsTo
+    {
+        return $this->belongsTo(Vendita::class);
+    }
 }
--- app/Models/Vendita.php
+++ app/Models/Vendita.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vendita extends Model
 {
@@ -38,5 +39,10 @@ class Vendita extends Model
     public function addetto(): BelongsTo
     {
         return $this->belongsTo(Addetto::class);
+    }
+
+    public function pagamenti(): HasMany
+    {
+        return $this->hasMany(Pagamento::class);
     }
 }
