5cbb1ef0bbdc67271640faff59c927f24bba2e30
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 15:19:34 +0200

    added relationship one to many between Attivita and Vendita

--- app/Models/Attivita.php
+++ app/Models/Attivita.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Attivita extends Model
 {
@@ -34,5 +35,10 @@ class Attivita extends Model
     public function ragioneSociale(): BelongsTo
     {
         return $this->belongsTo(RagioneSociale::class);
+    }
+
+    public function vendite(): HasMany
+    {
+        return $this->hasMany(Vendita::class);
     }
 }
--- app/Models/Vendita.php
+++ app/Models/Vendita.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vendita extends Model
 {
@@ -28,4 +29,9 @@ class Vendita extends Model
         'totale',
         'totale_imponibile',
     ];
+
+    public function attivita(): BelongsTo
+    {
+        return $this->belongsTo(Attivita::class);
+    }
 }
