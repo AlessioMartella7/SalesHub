f85e6f23cb34a9fec4fd0bb9f21ca8d04f9d424a
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 11:40:06 +0200

    added ArticoloDettaglio fillable

--- app/Models/ArticoloDettaglio.php
+++ app/Models/ArticoloDettaglio.php
@@ -11,4 +11,26 @@ class ArticoloDettaglio extends Model
     use HasFactory;
 
     protected $table = 'articoli_dettagli';
+
+    protected $fillable = [
+        'articolo_id',
+        'tipologia_vendita',
+        'canone',
+        'prezzo',
+        'aliquota_prezzo',
+        'natura',
+        'importo_imponibile',
+        'sconto',
+        'sconto_iva_esclusa',
+        'importo_anticipo',
+        'importo_finanziato',
+        'importo_credito',
+        'importo_ndc',
+        'importo_scontrino',
+        'vendita_info1',
+        'vendita_info2',
+        'vendita_info3',
+        'vendita_info4',
+        'vendita_info5',
+    ];
 }
