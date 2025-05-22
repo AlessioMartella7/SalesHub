f50f97e2b67355c3d3b4ae5e30c71e6b925b9da0
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 15:36:20 +0200

    added relationshiop many to many between Attivita and Addetto

--- app/Models/Attivita.php
+++ app/Models/Attivita.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Attivita extends Model
@@ -40,5 +41,10 @@ class Attivita extends Model
     public function vendite(): HasMany
     {
         return $this->hasMany(Vendita::class);
+    }
+
+    public function addetti(): BelongsToMany
+    {
+        return $this->belongsToMany(Addetto::class);
     }
 }
