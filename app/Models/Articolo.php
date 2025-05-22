9b2d16ca08642cda7b5da8bd5a86d21682f9d14d
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 16:19:47 +0200

    added relationship one to many between Categoria and Articolo

--- app/Models/Articolo.php
+++ app/Models/Articolo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -38,5 +39,10 @@ class Articolo extends Model
     public function articoliDettagli(): HasMany
     {
         return $this->hasMany(ArticoloDettaglio::class);
+    }
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
     }
 }
