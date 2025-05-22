eb0d1cddf3fa1b36895778b8efce8c4f83d3fe9a
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-22 10:16:44 +0200

    created -mcsfr for given migrations

---
+++ app/Http/Controllers/VenditaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vendita;
+use App\Http\Requests\StoreVenditaRequest;
+use App\Http\Requests\UpdateVenditaRequest;
+
+class VenditaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVenditaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vendita $vendita)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vendita $vendita)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVenditaRequest $request, Vendita $vendita)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vendita $vendita)
+    {
+        //
+    }
+}
---
+++ database/seeders/RagioneSocialeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RagioneSocialeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
