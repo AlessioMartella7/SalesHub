7c726fd4bdfe3f9a87976bfd4d9899bc7df20f7f
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 15:42:39 +0200

    added Attivita service

--- app/Services/Vendita/SubServices/CreateAttivitaService.php
+++ app/Services/Vendita/SubServices/CreateAttivitaService.php
@@ -4,3 +4,14 @@ namespace App\Services\Vendita\SubServices;
 
 use App\Models\Attivita;
 use App\Models\RagioneSociale;
+
+class CreateAttivitaService
+{
+    public function store(array $data, RagioneSociale $ragioneSociale): Attivita
+    {
+        return Attivita::firstOrCreate(
+            ['codice_esterno' => $data['codice_esterno']],
+            array_merge($data, ['ragione_sociale_id' => $ragioneSociale->id])
+        );
+    }
+}
