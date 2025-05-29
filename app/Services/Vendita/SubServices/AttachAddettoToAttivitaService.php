3a2f1bad7ba3a9568e393248480de8ed5c3296aa
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 16:21:29 +0200

    added AddettoAttivita service

--- app/Services/Vendita/SubServices/AttachAddettoToAttivitaService.php
+++ app/Services/Vendita/SubServices/AttachAddettoToAttivitaService.php
@@ -4,3 +4,11 @@ namespace App\Services\Vendita\SubServices;
 
 use App\Models\Addetto;
 use App\Models\Attivita;
+
+class AttachAddettoToAttivitaService
+{
+    public function attach(Addetto $addetto, Attivita $attivita): void
+    {
+        $attivita->addetti()->syncWithoutDetaching([$addetto->id]);
+    }
+}
