5edd3c911989540a1e8add3ca086b6e971a88799
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 18:00:11 +0200

    added ArticoliVendita service

--- app/Services/Vendita/SubServices/AttachArticoliToVenditaService.php
+++ app/Services/Vendita/SubServices/AttachArticoliToVenditaService.php
@@ -2,5 +2,16 @@
 
 namespace App\Services\Vendita\SubServices;
 
-use App\Models\Articolo;
 use App\Models\Vendita;
+
+class AttachArticoliToVenditaService
+{
+    public function attach(Vendita $vendita, array $articoli): void
+    {
+        foreach ($articoli as $item) {
+            $vendita->articoli()->attach($item['id_articolo'], [
+                'canone' => $item['canone'] ?? null
+            ]);
+        }
+    }
+}
