e8105fd5d944d848b06ef3a6fa202464d41346a4
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 15:48:02 +0200

    added Addetto service

--- app/Services/Vendita/SubServices/CreateAddettoService.php
+++ app/Services/Vendita/SubServices/CreateAddettoService.php
@@ -3,3 +3,14 @@
 namespace App\Services\Vendita\SubServices;
 
 use App\Models\Addetto;
+
+class CreateAddettoService
+{
+    public function store(array $data): Addetto
+    {
+        return Addetto::firstOrCreate(
+            ['codice_esterno' => $data['codice_esterno']],
+            $data
+        );
+    }
+}
