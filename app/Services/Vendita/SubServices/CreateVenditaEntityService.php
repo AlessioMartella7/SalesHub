167950f0924a6ac9059c89f1655cb4b7857560ff
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 17:44:21 +0200

    added VenditaEntity service

--- app/Services/Vendita/SubServices/CreateVenditaEntityService.php
+++ app/Services/Vendita/SubServices/CreateVenditaEntityService.php
@@ -6,3 +6,15 @@ use App\Models\Vendita;
 use App\Models\Cliente;
 use App\Models\Addetto;
 use App\Models\Attivita;
+
+class CreateVenditaEntityService
+{
+    public function create(array $data, Cliente $cliente, Addetto $addetto, Attivita $attivita): Vendita
+    {
+        return Vendita::create(array_merge($data, [
+            'cliente_id' => $cliente->id,
+            'addetto_id' => $addetto->id,
+            'attivita_id' => $attivita->id,
+        ]));
+    }
+}
