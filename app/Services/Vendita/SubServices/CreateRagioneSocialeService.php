a3a6b8f4a1be7ff740bcac176c56c42e14c8fbfb
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 15:17:06 +0200

    added RagioneSociale service

--- app/Services/Vendita/SubServices/CreateRagioneSocialeService.php
+++ app/Services/Vendita/SubServices/CreateRagioneSocialeService.php
@@ -4,3 +4,14 @@ namespace App\Services\Vendita\SubServices;
 
 use App\Models\Organizzazione;
 use App\Models\RagioneSociale;
+
+class CreateRagioneSocialeService
+{
+    public function create(array $data, Organizzazione $organizzazione): RagioneSociale
+    {
+        return RagioneSociale::firstOrCreate(
+            ['codice_esterno' => $data['codice_esterno']],
+            array_merge($data, ['organizzazione_id' => $organizzazione->id])
+        );
+    }
+}
