cd6c37d6b1c043eb32c604da20dcb7a29dd73b7b
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 17:30:07 +0200

    added ArticoloDettaglio service

--- app/Services/Vendita/SubServices/CreateArticoloDettaglioService.php
+++ app/Services/Vendita/SubServices/CreateArticoloDettaglioService.php
@@ -4,3 +4,13 @@ namespace App\Services\Vendita\SubServices;
 
 use App\Models\ArticoloDettaglio;
 use App\Models\Articolo;
+
+class CreateArticoloDettaglioService
+{
+    public function createMany(array $dettagli, Articolo $articolo): void
+    {
+        foreach ($dettagli as $dettaglio) {
+            $articolo->articoliDettagli()->create($dettaglio);
+        }
+    }
+}
