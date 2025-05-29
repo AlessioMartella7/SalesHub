180854f4683ee92b0e5878d2c8b011f3dd16a6e1
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 17:01:57 +0200

    added Tipologia service

--- app/Services/Vendita/SubServices/CreateTipologiaService.php
+++ app/Services/Vendita/SubServices/CreateTipologiaService.php
@@ -4,3 +4,14 @@ namespace App\Services\Vendita\SubServices;
 
 use App\Models\Tipologia;
 use App\Models\Categoria;
+
+class CreateTipologiaService
+{
+    public function create(array $data, Categoria $categoria): Tipologia
+    {
+        return Tipologia::firstOrCreate(
+            ['tipologia' => $data['tipologia'], 'categoria_id' => $categoria->id],
+            array_merge($data, ['categoria_id' => $categoria->id])
+        );
+    }
+}
