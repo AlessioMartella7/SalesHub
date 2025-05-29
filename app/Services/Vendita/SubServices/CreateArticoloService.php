6b6ceff393b51f2387c366e0b71a7d6f78d48964
AlessioMartella7 <alessio.mart87@gmail.com>
2025-05-29 17:12:02 +0200

    added Articolo service

--- app/Services/Vendita/SubServices/CreateArticoloService.php
+++ app/Services/Vendita/SubServices/CreateArticoloService.php
@@ -5,3 +5,16 @@ namespace App\Services\Vendita\SubServices;
 use App\Models\Articolo;
 use App\Models\Categoria;
 use App\Models\Tipologia;
+
+class CreateArticoloService
+{
+    public function create(array $data, Categoria $categoria, Tipologia $tipologia): Articolo
+    {
+        return Articolo::create(
+            array_merge($data, [
+                'categoria_id' => $categoria->id,
+                'tipologia_id' => $tipologia->id,
+            ])
+        );
+    }
+}
