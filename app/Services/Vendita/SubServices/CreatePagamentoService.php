31cb61b9827c8f6b6ca94c8d678a6cca39077708
Alessio_Martella <a.martella@sctechnology.it>
2025-05-30 11:08:39 +0200

    added Pagamento service

--- app/Services/Vendita/SubServices/CreatePagamentoService.php
+++ app/Services/Vendita/SubServices/CreatePagamentoService.php
@@ -4,3 +4,13 @@ namespace App\Services\Vendita\SubServices;
 
 use App\Models\Vendita;
 use App\Models\Pagamento;
+
+class CreatePagamentoService
+{
+    public function createMany(array $pagamenti, Vendita $vendita): void
+    {
+       foreach($pagamenti as $datiPagamento) {
+        $vendita->pagamenti()->create($datiPagamento);
+       }
+    }
+}
