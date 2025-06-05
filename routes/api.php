ce4210e6486cf95ae0d7f6711252dc81d8806967
AlessioMartella7 <alessio.mart87@gmail.com>
2025-06-05 15:43:59 +0200

    added VenditaController API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\VenditaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+//Rotte API Vendite
+
+Route::middleware('auth:api')
+    ->controller(VenditaController::class)
+    ->prefix('vendite')
+    ->name('vendite.')
+    ->group(function () {
+        Route::post('/', 'store')->name('store');
+    });
