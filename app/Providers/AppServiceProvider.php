82fbc41a49d0d2c180c3380d6d65380cb9d6541e
Alessio_Martella <a.martella@sctechnology.it>
2025-06-24 10:59:46 +0200

    changed redirect after auth in appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RedirectIfAuthenticated::redirectUsing(fn () => route('fissi.index'));
+        RedirectIfAuthenticated::redirectUsing(fn () => route('vendite.index'));
 
         Paginator::useBootstrap();
 
