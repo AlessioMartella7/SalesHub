4eb3fde47b0743c2b369f18d66d832bf70a0efc9
Alessio_Martella <a.martella@sctechnology.it>
2025-07-10 16:17:33 +0200

    added ProcessVenditeChunk Job

---
+++ app/Jobs/ProcessVenditeChunk.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Vendita\CreateVenditaService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessVenditeChunkJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public array $venditeChunk;
+
+    public function __construct(array $venditeChunk)
+    {
+        $this->venditeChunk = $venditeChunk;
+    }
+
+    public function handle(CreateVenditaService $service)
+    {
+        foreach ($this->venditeChunk as $data) {
+            try {
+                $service->handle($data);
+            } catch (\Throwable $e) {
+                \Log::error('Errore durante l\'elaborazione della vendita:', [
+                    'message' => $e->getMessage(),
+                    'data' => $data,
+                    'exception' => config('app.debug') ? [
+                        'errorInfo' => $e->getTrace(),
+                        'code' => $e->getCode(),
+                    ] : null,
+                ]);
+                throw $e;
+            }
+        }
+    }
+}
