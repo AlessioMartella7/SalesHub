ba9804c6309a35f573ede53b1bfb54287fffa9d6
Alessio_Martella <a.martella@sctechnology.it>
2025-08-07 12:46:02 +0200

    imported classes for PowerBIController

--- app/Http/Controllers/Api/PowerBIController.php
+++ app/Http/Controllers/Api/PowerBIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PowerBIController extends Controller
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\FissoController;
+use App\Http\Controllers\api\PowerBIController;
 use App\Http\Controllers\Api\TokenController;
 use App\Http\Controllers\Api\VenditaController;
 use App\Http\Middleware\GoogleTokenAuth;
