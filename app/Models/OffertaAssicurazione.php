30ce2b65ac3403718f6d5730de321757e8e64ca6
Alessio_Martella <a.martella@sctechnology.it>
2025-09-16 11:41:05 +0200

    created "OffertaAssicurazione" model

---
+++ app/Models/OffertaAssicurazione.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OffertaAssicurazione extends Model
+{
+    protected $table = 'offerta_assicurazioni';
+
+    protected $fillable = [
+        'user_id',
+        'codice_pdv',
+        'codice_contratto',
+        'id_carrello',
+        'venditore',
+        'stato_contratto',
+        'attivato',
+        'metodo_pagamento',
+        'dt_inserimento',
+        'dt_primo_pagamento',
+        'dt_cancellazione',
+        'categoria',
+        'pacchetto',
+        'esito_carrello',
+        'causale_cancellazione'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
