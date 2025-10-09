23bf7e0e12123a1e9aa6ef8e28fb6b250a0aa9cd
Alessio_Martella <a.martella@sctechnology.it>
2025-10-09 17:04:30 +0200

    changed rules to accept negative value in ArticoloDettaglioRules

--- app/Http/Requests/Supports/Rules/ArticoloDettaglioRules.php
+++ app/Http/Requests/Supports/Rules/ArticoloDettaglioRules.php
@@ -10,20 +10,20 @@ class ArticoloDettaglioRules
             'articoli.*.dettaglio.tipologia_vendita' => ['nullable', 'string'],
             'articoli.*.dettaglio.canone' => ['nullable', 'string', 'max:10'],
 
-            'articoli.*.dettaglio.prezzo' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
-            'articoli.*.dettaglio.aliquota_prezzo' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
-            'articoli.*.dettaglio.natura' => ['nullable', 'string', 'max:10'],
-            'articoli.*.dettaglio.importo_imponibile' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
+            'articoli.*.dettaglio.prezzo' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
+            'articoli.*.dettaglio.aliquota_prezzo' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
+            'articoli.*.dettaglio.natura' => ['nullable', 'string', 'max:1-0.9'],
+            'articoli.*.dettaglio.importo_imponibile' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
 
-            'articoli.*.dettaglio.sconto' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
-            'articoli.*.dettaglio.sconto_iva_esclusa' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
+            'articoli.*.dettaglio.sconto' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
+            'articoli.*.dettaglio.sconto_iva_esclusa' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
 
-            'articoli.*.dettaglio.importo_anticipo' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
-            'articoli.*.dettaglio.importo_finanziato' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
-            'articoli.*.dettaglio.importo_credito' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
+            'articoli.*.dettaglio.importo_anticipo' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
+            'articoli.*.dettaglio.importo_finanziato' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
+            'articoli.*.dettaglio.importo_credito' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
             'articoli.*.dettaglio.importo_ndc' => ['nullable', 'numeric'],
 
-            'articoli.*.dettaglio.importo_scontrino' => ['nullable', 'numeric', 'between:0,99999999999999999999.99'],
+            'articoli.*.dettaglio.importo_scontrino' => ['nullable', 'numeric', 'between:-0.9,99999999999999999999.99'],
 
             'articoli.*.dettaglio.vendita_info1' => ['nullable', 'string', 'max:255'],
             'articoli.*.dettaglio.vendita_info2' => ['nullable', 'string', 'max:255'],
